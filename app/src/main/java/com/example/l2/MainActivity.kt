80719a8baaf8de9dc6d569cddc229d4baa835d28
Santosh Mehta <santosh@goodeducator.com>
2021-08-18 11:13:13 +0530

    restricted webview to open external url into same webview/app

--- app/src/main/java/com/example/l2/MainActivity.kt
+++ app/src/main/java/com/example/l2/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.l2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.databinding.DataBindingUtil
 import com.example.l2.databinding.ActivityMainBinding
 
@@ -12,7 +15,25 @@ class MainActivity : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         supportActionBar?.hide()
         binding.webView.apply {
-            loadUrl("http://15.206.124.148/")
+            this.canGoBack()
+            this.loadUrl("http://15.206.124.148/")
+            this.webViewClient = object : WebViewClient() {
+                override fun shouldOverrideUrlLoading(
+                    view: WebView?,
+                    request: WebResourceRequest?
+                ): Boolean {
+                    view?.loadUrl(request?.url.toString())
+                    return false
+                }
+            }
+        }
+    }
+
+    override fun onBackPressed() {
+        if (binding.webView.canGoBack()) {
+            binding.webView.goBack()
+        } else {
+            super.onBackPressed()
         }
     }
 }
